14aaa1ea60768e813defc6ba31fe9e4236042df4
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-03-10 17:41:23 -0300

    Resolvido Problema do Laravel nao ler .env

---
+++ database/migrations/2021_03_10_190511_create_table_series.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableSeries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->date('release-year');
+            $table->date('end-year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
