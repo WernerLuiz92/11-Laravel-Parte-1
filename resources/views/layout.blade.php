681351031e2e491f97c00733b5451df20cacbd7b
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-03-10 15:34:29 -0300

    Criando rota / e view para a home, alteraçao do layout.blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,7 +18,7 @@
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Laravel MVC</a>
+            <a class="navbar-brand" href="/">Laravel MVC</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -26,7 +26,7 @@
             <div class="collapse navbar-collapse" id="navbarsExample04">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/series">Listar Séries</a>
