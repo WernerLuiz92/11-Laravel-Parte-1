6ec14943c22a8c1bd8fd78a59f2e22d0ee7b39a5
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-03-18 13:14:21 -0300

    Alteração visual View temporadas

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -4,7 +4,7 @@
     <h1>{{ $serieTitle }}</h1>
     <div class="row row-cols-auto">
         @foreach ($seasons as $season)
-            <div class="card col ms-3 mt-3">
+            <div class="card col col-2 ms-3 mt-3">
                 <div class="card-body">
                     <h5 class="card-title">Temporada {{ $season->number }}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Ano: {{ $season->releaseDate ?? '' }}</h6>
