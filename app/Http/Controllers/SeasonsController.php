90c4954f72cb54d1940ece95d8f399820733b951
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-03-25 18:14:26 -0300

    feat: Configuração do app.js para trabalhar com o timeago.js feat: alteração do SeasonsController para exibir novas informações do Banco de Dados

--- app/Http/Controllers/SeasonsController.php
+++ app/Http/Controllers/SeasonsController.php
@@ -3,20 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
+use Illuminate\Support\Facades\DB;
 
 class SeasonsController extends Controller
 {
     public function index(int $serieId)
     {
-        $seasons = Serie::find($serieId)->seasons;
-
+        $serie = Serie::find($serieId);
+        $seasons = $serie->seasons;
         $pageTitle = 'Lista de Temporadas';
-        $serieTitle = Serie::find($serieId)->title;
+        $serieTitle = $serie->title;
+        $qtyEpisodes = [];
+        $seasonYear = [];
+        $serieImgUrl = $serie->imageUrl;
+        $serieSynopsis = $serie->synopsis;
+        foreach ($seasons as $season) {
+            $qtyEpisodes[$season->id] = $season->episodes->count();
+            $seasonYear[$season->id] = date('Y', strtotime($season->releaseDate));
+        }
+
+        $serieLastEpisode = DB::table('series')
+                            ->join('seasons', 'series.id', '=', 'seasons.serie_id')
+                            ->join('episodes', 'seasons.id', '=', 'episodes.season_id')
+                            ->select('episodes.releaseDate')
+                            ->where('series.id', $serieId)
+                            ->get();
+
+        dd($serieLastEpisode);
+        exit();
+
+        $serieNextEpisode = DB::table('episodes')
+                            ->where('releaseDate', '>', date('Y-m-d'))
+                            ->min('releaseDate');
 
         return view('seasons.index', compact(
             'seasons',
             'pageTitle',
             'serieTitle',
+            'seasonYear',
+            'qtyEpisodes',
+            'serieImgUrl',
+            'serieSynopsis',
+            'serieLastEpisode',
+            'serieNextEpisode',
         ));
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import { format, render, cancel, register } from 'timeago.js';
+import pt_BR from 'timeago.js/lib/lang/pt_BR';
+register('pt_BR', pt_BR);
+
+const nodes = document.querySelectorAll('.timeago');
+
+if (nodes.length > 0) {
+    render(nodes, 'pt_BR');
+}
